52eef1e83d3714355eee9b7b8e56f4b925cc9093
Subhadeep Bandyopadhyay <subhadeep.bandyopadhyay.17@mountblue.tech>
2021-09-22 13:53:12 +0530

    usinghof

---
+++ src/public/viz.js
@@ -0,0 +1,276 @@
+fetch("./output/numberofmatchesPlayed.json")
+  .then((response) => response.json())
+  .then((JSONData) => viz1(JSONData));
+function viz1(JSONData) {
+  Highcharts.chart("numberofmatchesPlayed", {
+    title: {
+      text: "1. Matches Played per Season in IPL",
+    },
+    chart: {
+      type: "column",
+      zoomType: "x",
+      borderRadius: 20,
+      backgroundColor: "#F5FFFA",
+    },
+    credits: {
+      enabled: false,
+    },
+    tooltip: {
+      backgroundColor: "#000000",
+      borderRadius: 20,
+      style: {
+        color: "#ffffff",
+      },
+    },
+    xAxis: {
+      title: {
+        text: "Years",
+        style: {
+          color: "#405795",
+        },
+      },
+      labels: {
+        style: {
+          color: "#405795",
+        },
+      },
+      categories: JSONData.map(({ year }) => year),
+    },
+    yAxis: {
+      title: {
+        text: "Matches",
+        style: {
+          color: "#405795",
+        },
+      },
+      labels: {
+        style: {
+          color: "#405795",
+        },
+      },
+    },
+    series: [
+      {
+        name: "Matches",
+        data: JSONData.map(({ matches }) => matches),
+        color: "#87B41C",
+        borderRadius: 10,
+      },
+    ],
+  });
+}
+
+fetch("./output/numberofmatchesWon.json")
+  .then((response) => response.json())
+  .then((JSONData) => viz2(JSONData));
+function viz2(JSONData) {
+  const teamsAndyears = JSONData.reduce(
+    (subject, { team, year }) => {
+      if (!subject[0].includes(team)) {
+        subject[0].push(team);
+      }
+      if (!subject[1].includes(year)) {
+        subject[1].push(year);
+      }
+      return subject;
+    },
+    [[], []]
+  );
+
+  const newSeries = [];
+  teamsAndyears[0].forEach((currentTeam) => {
+    let series2Push = {
+      name: currentTeam,
+      data: [],
+    };
+    let wins2Push = 0;
+    teamsAndyears[1].forEach((currentYear) => {
+      JSONData.forEach(({ year, team, wins }) => {
+        if (team === currentTeam && year === currentYear) {
+          wins2Push = wins;
+        }
+      });
+      series2Push["data"].push(wins2Push);
+    });
+    newSeries.push(series2Push);
+  });
+
+  Highcharts.chart("numberofmatchesWon", {
+    title: {
+      text: "2. Matches Won per Team per Year in IPL",
+    },
+    plotOptions: {
+      column: {
+        stacking: "normal",
+      },
+    },
+    chart: {
+      type: "column",
+      zoomType: "x",
+      borderRadius: 20,
+      backgroundColor: "#F5FFFA",
+    },
+    credits: {
+      enabled: false,
+    },
+    tooltip: {
+      backgroundColor: "#333333",
+      borderRadius: 20,
+      style: {
+        color: "#ffffff",
+      },
+    },
+    xAxis: {
+      title: {
+        text: "Years",
+        style: {
+          color: "#405795",
+        },
+      },
+      labels: {
+        style: {
+          color: "#405795",
+        },
+      },
+      categories: teamsAndyears[1],
+    },
+    yAxis: {
+      title: {
+        text: "Wins",
+        style: {
+          color: "#405795",
+        },
+      },
+      labels: {
+        style: {
+          color: "#405795",
+        },
+      },
+    },
+    series: newSeries,
+  });
+}
+
+fetch("./output/extraRuns2016.json")
+  .then((response) => response.json())
+  .then((JSONData) => viz3(JSONData));
+function viz3(JSONData) {
+  Highcharts.chart("extraRuns2016", {
+    title: {
+      text: "3. Extra Runs conceded per Team in the Year 2016",
+    },
+    chart: {
+      type: "column",
+      zoomType: "x",
+      borderRadius: 20,
+      backgroundColor: "#F5FFFA",
+    },
+    credits: {
+      enabled: false,
+    },
+    tooltip: {
+      backgroundColor: "#333333",
+      borderRadius: 20,
+      style: {
+        color: "#ffffff",
+      },
+    },
+    xAxis: {
+      title: {
+        text: "Teams",
+        style: {
+          color: "#405795",
+        },
+      },
+      labels: {
+        style: {
+          color: "#405795",
+        },
+      },
+      categories: JSONData.map(({ team }) => team),
+    },
+    yAxis: {
+      title: {
+        text: "Extra Runs",
+        style: {
+          color: "#405795",
+        },
+      },
+      labels: {
+        style: {
+          color: "#405795",
+        },
+      },
+    },
+    series: [
+      {
+        name: "Extra Runs",
+        data: JSONData.map(({ extra_runs }) => extra_runs),
+        color: "#87B41C",
+        borderRadius: 10,
+      },
+    ],
+  });
+}
+
+fetch("./output/economicalBowlers2015.json")
+  .then((response) => response.json())
+  .then((JSONData) => viz4(JSONData));
+function viz4(JSONData) {
+  Highcharts.chart("economicalBowlers2015", {
+    title: {
+      text: "4. Top 10 Economical Bowlers in the Year 2015",
+    },
+    chart: {
+      type: "column",
+      zoomType: "x",
+      borderRadius: 20,
+      backgroundColor: "#F5FFFA",
+    },
+    credits: {
+      enabled: false,
+    },
+    tooltip: {
+      backgroundColor: "#333333",
+      borderRadius: 20,
+      style: {
+        color: "#ffffff",
+      },
+    },
+    xAxis: {
+      title: {
+        text: "Bowlers",
+        style: {
+          color: "#405795",
+        },
+      },
+      labels: {
+        style: {
+          color: "#405795",
+        },
+      },
+      categories: JSONData.map(({ bowler }) => bowler),
+    },
+    yAxis: {
+      title: {
+        text: "Economy",
+        style: {
+          color: "#405795",
+        },
+      },
+      labels: {
+        style: {
+          color: "#405795",
+        },
+      },
+    },
+    series: [
+      {
+        name: "Economy",
+        data: JSONData.map(({ economy }) => economy),
+        color: "#87B41C",
+        borderRadius: 10,
+      },
+    ],
+  });
+}
