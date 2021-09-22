d29c752dcf3d8d2725961ea21d64140ca4bb74ff
Subhadeep Bandyopadhyay <subhadeep.bandyopadhyay.17@mountblue.tech>
2021-09-22 13:55:13 +0530

    remove useless spaces

--- src/server/ipl.js
+++ src/server/ipl.js
@@ -1,124 +1,124 @@
 //Problem 1:   Number of matches played per year for all the years in IPL.
 
-function numberofmatchesPlayed(matches) 
-{    
-    const result = {};
-  
-    for (let match of matches) {
-        const season = match.season;
-        if (result[season]) {         
-            result[season] += 1;
-        } else {
-            result[season] = 1;
-        }
-    }    
-    return result;
+function numberofmatchesPlayed(matches) {
+  const result = {};
+
+  for (let match of matches) {
+    const season = match.season;
+    if (result[season]) {
+      result[season] += 1;
+    } else {
+      result[season] = 1;
+    }
+  }
+  return result;
 }
-
-
 
 //Problem 2:   Number of matches won per team per year in IPL.
 
-function numberofmatchesWon(matches)    
-{
-    let result = {};
-    
-    for(let match of matches) {
-        if (result.hasOwnProperty(match.winner)) {
-            for (let team of result[match.winner]) {
-                if (team.hasOwnProperty(match.season)) {
-                     team[match.season] += 1;
-                }
-                else {
-                    team[match.season] = 1;
-                }
-            }
+function numberofmatchesWon(matches) {
+  let result = {};
+
+  for (let match of matches) {
+    if (result.hasOwnProperty(match.winner)) {
+      for (let team of result[match.winner]) {
+        if (team.hasOwnProperty(match.season)) {
+          team[match.season] += 1;
+        } else {
+          team[match.season] = 1;
         }
-        else {
-            result1 = {};
-            result[match.winner] = [];
-            result1[match.season] = 1;
-            result[match.winner].push(result1);
-        }
-    }    
-    return result;
+      }
+    } else {
+      result1 = {};
+      result[match.winner] = [];
+      result1[match.season] = 1;
+      result[match.winner].push(result1);
+    }
+  }
+  return result;
 }
-
-
 
 //Problem 3:   Extra runs conceded per team in the year 2016.
 
-function extraRuns2016(matches, deliveries) 
-{
-    let result = {};
+function extraRuns2016(matches, deliveries) {
+  let result = {};
 
-    for (let delivery of deliveries){
-        if (delivery.extra_runs != 0) {
-            for(let match of matches){
-                if (match.id === delivery.match_id) {
-                    if (match.season === "2016") {
-                        if (result.hasOwnProperty(delivery.bowling_team)) {
-                            result[delivery.bowling_team] += Number(delivery.extra_runs);
-                        }
-                        else {
-                            result[delivery.bowling_team] = Number(delivery.extra_runs);
-                        }
-                    }
-                }
+  for (let delivery of deliveries) {
+    if (delivery.extra_runs != 0) {
+      for (let match of matches) {
+        if (match.id === delivery.match_id) {
+          if (match.season === "2016") {
+            if (result.hasOwnProperty(delivery.bowling_team)) {
+              result[delivery.bowling_team] += Number(delivery.extra_runs);
+            } else {
+              result[delivery.bowling_team] = Number(delivery.extra_runs);
             }
+          }
         }
+      }
     }
-    
-    return result;
+  }
+
+  return result;
 }
-
-
 
 //Problem 4:   Top 10 economical bowlers in the year 2015.
 
-function economicalBowlers2015(matches, deliveries) 
-{
-    let matchId = [];
+function economicalBowlers2015(matches, deliveries) {
+  let matchId = [];
 
-    for (let match of matches) {
-        const season = match.season;
-        if (season === '2015') {
-            matchId.push(match.id);
-        }
+  for (let match of matches) {
+    const season = match.season;
+    if (season === "2015") {
+      matchId.push(match.id);
     }
-    const runsByPlayer = {};
-    const ballsPerPlayer = {};
-    
-    for(let delivery of deliveries){
-        if(matchId.includes(delivery.match_id)){
-            if(!runsByPlayer.hasOwnProperty(delivery.bowler)){
-                runsByPlayer[delivery.bowler] = Number(delivery.total_runs);
-            }else{
-                runsByPlayer[delivery.bowler] += Number(delivery.total_runs);
-            }
-            if(!ballsPerPlayer.hasOwnProperty(delivery.bowler)){
-                ballsPerPlayer[delivery.bowler] = 1;
-            }else{
-                ballsPerPlayer[delivery.bowler] += 1;
-            }
-            if(Number(delivery.wide_runs) > 0 || Number(delivery.noball_runs) > 0){
-                ballsPerPlayer[delivery.bowler] -= 1;
-            }
-        }
-    }
+  }
+  const runsByPlayer = {};
+  const ballsPerPlayer = {};
 
-    let array = [];
-    for(let run in runsByPlayer){
-        array.push([run, Number(Number.parseFloat(runsByPlayer[run] / (ballsPerPlayer[run] / 6)).toFixed(2))]);
+  for (let delivery of deliveries) {
+    if (matchId.includes(delivery.match_id)) {
+      if (!runsByPlayer.hasOwnProperty(delivery.bowler)) {
+        runsByPlayer[delivery.bowler] = Number(delivery.total_runs);
+      } else {
+        runsByPlayer[delivery.bowler] += Number(delivery.total_runs);
+      }
+      if (!ballsPerPlayer.hasOwnProperty(delivery.bowler)) {
+        ballsPerPlayer[delivery.bowler] = 1;
+      } else {
+        ballsPerPlayer[delivery.bowler] += 1;
+      }
+      if (Number(delivery.wide_runs) > 0 || Number(delivery.noball_runs) > 0) {
+        ballsPerPlayer[delivery.bowler] -= 1;
+      }
     }
-    array.sort((a, b) => {return a[1] - b[1]});
-    let result = [];
-    for(let index = 0; index < 10; index++){
-        result.push({'bowler' : array[index][0], 'economy' : array[index][1]});
-        // result.push(array[index][0]);
-    }    
-    return result;
+  }
+
+  let array = [];
+  for (let run in runsByPlayer) {
+    array.push([
+      run,
+      Number(
+        Number.parseFloat(
+          runsByPlayer[run] / (ballsPerPlayer[run] / 6)
+        ).toFixed(2)
+      ),
+    ]);
+  }
+  array.sort((a, b) => {
+    return a[1] - b[1];
+  });
+  let result = [];
+  for (let index = 0; index < 10; index++) {
+    result.push({ bowler: array[index][0], economy: array[index][1] });
+    // result.push(array[index][0]);
+  }
+  return result;
 }
 
-
-module.exports = {numberofmatchesPlayed, numberofmatchesWon, extraRuns2016, economicalBowlers2015};
+module.exports = {
+  numberofmatchesPlayed,
+  numberofmatchesWon,
+  extraRuns2016,
+  economicalBowlers2015,
+};
